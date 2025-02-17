c57bad04819a0b0a36b3533338225549e281d99f
nielitton <nieliton.sousa3040@gmail.com>
2025-02-17 14:38:18 -0300

    feat: skeleton loading

--- src/components/lists/notes-list.tsx
+++ src/components/lists/notes-list.tsx
@@ -8,11 +8,12 @@ import { useNoteStore } from "@/stores/notes-store"
 import { AnimatePresence, motion } from "framer-motion"
 import { Pen, Trash2 } from "lucide-react"
 import { useEffect } from "react"
+import { Skeleton } from "../ui/skeleton"
 
 export default function NotesList() {
     const { notes, setNotes } = useNoteStore()
     const { mutate: deleteNoteMutate } = UseDeleteNote()
-    const { data: notesFinded, refetch } = UseGetNotes()
+    const { data: notesFinded, refetch, isLoading } = UseGetNotes()
     const { setModalIsOpen, setModalType, setNoteId } = useModalStore()
 
     useEffect(() => {
@@ -33,39 +34,50 @@ export default function NotesList() {
     return (
         <div className="p-6">
             <AnimatePresence>
-                {notes?.map((note) => (
-                    <motion.div
-                        key={note.id}
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -20 }}
-                        transition={{ duration: 0.2 }}
-                        className="bg-gray-100 p-4 rounded-lg space-y-2 mb-4 border-l-4 border-warlocks-blue"
-                    >
-                        <div className="flex justify-between items-center">
-                            <h3 className="font-semibold text-warlocks-blue">{note.title}</h3>
-                            <div className="flex">
-                                <Button
-                                    variant="ghost"
-                                    size="icon"
-                                    onClick={() => handleEditModal(note.id)}
-                                    className="text-warlocks-blue hover:bg-warlocks-blue/10 flex"
-                                >
-                                    <Pen className="h-4 w-4" />
-                                </Button>
-                                <Button
-                                    variant="ghost"
-                                    size="icon"
-                                    onClick={() => deleteNote(note.id)}
-                                    className="text-red-600 hover:bg-warlocks-blue/10"
-                                >
-                                    <Trash2 className="h-4 w-4" />
-                                </Button>
-                            </div>
+                {
+                    isLoading ?
+                        <div className="p-6 space-y-4">
+                            <Skeleton className="h-12 w-full" />
+                            <Skeleton className="h-12 w-full" />
+                            <Skeleton className="h-12 w-full" />
                         </div>
-                        <p className="text-sm text-gray-600">{note.content}</p>
-                    </motion.div>
-                ))}
+                        :
+                        <div>
+                            {notes?.map((note) => (
+                                <motion.div
+                                    key={note.id}
+                                    initial={{ opacity: 0, y: 20 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: -20 }}
+                                    transition={{ duration: 0.2 }}
+                                    className="bg-gray-100 p-4 rounded-lg space-y-2 mb-4 border-l-4 border-warlocks-blue"
+                                >
+                                    <div className="flex justify-between items-center">
+                                        <h3 className="font-semibold text-warlocks-blue">{note.title}</h3>
+                                        <div className="flex">
+                                            <Button
+                                                variant="ghost"
+                                                size="icon"
+                                                onClick={() => handleEditModal(note.id)}
+                                                className="text-warlocks-blue hover:bg-warlocks-blue/10 flex"
+                                            >
+                                                <Pen className="h-4 w-4" />
+                                            </Button>
+                                            <Button
+                                                variant="ghost"
+                                                size="icon"
+                                                onClick={() => deleteNote(note.id)}
+                                                className="text-red-600 hover:bg-warlocks-blue/10"
+                                            >
+                                                <Trash2 className="h-4 w-4" />
+                                            </Button>
+                                        </div>
+                                    </div>
+                                    <p className="text-sm text-gray-600">{note.content}</p>
+                                </motion.div>
+                            ))}
+                        </div>
+                }
             </AnimatePresence>
             {notesFinded?.totalRecords === 0 && (
                 <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="text-center text-gray-500 mt-4">
